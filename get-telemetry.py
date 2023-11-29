6caf5afb7b1733faf839816473b62aa6416438ec
shaftoes <sshaftoe@voltus.co>
2023-11-29 10:34:38 -0500

    Code Review

--- get-telemetry.py
+++ get-telemetry.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import csv
 import requests
 import urllib
@@ -7,25 +8,13 @@ from datetime import datetime, timedelta
 MAX_REQUEST_SIZE_HOURS = 48
 SECONDS_IN_HOUR = 3600.0
 VOLTUS_API_URL = os.getenv("VOLTUS_API_URL", "https://api.voltus.co")
-VOLTUS_API_KEY = os.getenv("VOLTUS_API_KEY", "secret")
-SITE_ID = os.getenv("SITE_ID")
+VOLTUS_API_KEY = os.getenv("VOLTUS_API_KEY")
 
 s = requests.Session()
 s.headers.update({"X-Voltus-API-Key": VOLTUS_API_KEY, "Accept": "application/json"})
 
 
-def compute_current_end_time(current_start_time, end_time):
-    """
-    helper function for get_telemetry.
-    """
-    interval = end_time - current_start_time
-    if interval.total_seconds() / SECONDS_IN_HOUR > MAX_REQUEST_SIZE_HOURS:
-        return current_start_time + timedelta(hours=MAX_REQUEST_SIZE_HOURS)
-    else:
-        return end_time
-
-
-def get_telemetry(site_id, start_time, end_time):
+def _get_telemetry(site_id: str, start_time: datetime, end_time: datetime):
     """
     returns a list of telemetry readings, each one being represented as a dictionary with the following fields:
     {
@@ -44,7 +33,11 @@ def get_telemetry(site_id, start_time, end_time):
 
     current_start = start_time
     while current_start != end_time:
-        current_end = compute_current_end_time(current_start, end_time)
+        # compute end_time for request in this loop
+        current_end = end_time
+        interval = end_time - current_start
+        if interval.total_seconds() / SECONDS_IN_HOUR > MAX_REQUEST_SIZE_HOURS:
+            current_end = current_start + timedelta(hours=MAX_REQUEST_SIZE_HOURS)
 
         # make request
         path_params = {
@@ -71,19 +64,23 @@ def get_telemetry(site_id, start_time, end_time):
 
         # setup next iteration
         current_start = current_end
+
     return readings
 
 
 if __name__ == "__main__":
-    if SITE_ID is None:
-        raise Exception("SITE_ID environment variable is unset")
+    if len(sys.argv) != 2:
+        sys.stderr.write(f"USAGE: {os.path.basename(sys.argv[0])} <site id>\n")
+        sys.exit(1)
     if VOLTUS_API_KEY is None:
-        raise Exception("VOLTUS_API_KEY environment variable is unset")
+        sys.stderr.write("VOLTUS_API_KEY environment variable is unset")
+        sys.exit(1)
 
     start_time = datetime.utcnow() - timedelta(days=8)
     end_time = datetime.utcnow() - timedelta(days=1)
+    site_id = sys.argv[1]
 
-    readings = get_telemetry(SITE_ID, start_time, end_time)
+    readings = _get_telemetry(site_id, start_time, end_time)
 
     # write readings to csv
     with open("readings.csv", "w") as csvfile:
